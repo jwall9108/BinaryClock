b3e2d5d8460be8ed07460aa90a2454502bd0c869
jwall9108 <jwall9108@gmail.com>
2014-05-30 14:40:33 -0400

    Init

---
+++ BinaryClock/Circle.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Shapes;
+using System.Windows.Media;
+
+using System.Windows;
+
+namespace BinaryClock
+{
+     class Circle : Shape
+    {
+        public EllipseGeometry _Circle;
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double angle { get; set; }
+        public bool active { get; set; }
+
+        public Point Center
+        {
+            get
+            {
+                return new Point(X  , Y );
+            }
+            set
+            {
+                //if ((value > 0))
+                //{
+                //    this.Height = value;
+                //}
+            }
+        }
+        public double _Height
+        {
+            get
+            {
+                return this.Height;
+            }
+            set
+            {
+                if ((value > 0))
+                {
+                    this.Height = value;
+                }
+            }
+        }
+        public double _Width
+        {
+            get
+            {
+                return this.Width;
+            }
+            set
+            {
+                if ((value > 0))
+                {
+                    this.Width = value;
+                }
+            }
+        }
+
+
+
+        public Circle ()
+        {
+            _Circle = new EllipseGeometry();
+
+            this.Height = 25;
+            this.Width = 25;
+            this.Fill = Brushes.White;
+            this.Stroke = Brushes.Black;
+
+        }
+
+        protected override Geometry DefiningGeometry
+        {
+            get
+            {
+
+                _Circle.RadiusX = this.Width / 2;
+                _Circle.RadiusY = this.Height / 2;
+
+                return _Circle;
+            }
+        }
+                     
+        public override Transform GeometryTransform
+        {
+            get
+            {
+                return Transform.Identity;
+            }
+        }
+
+
+        public void toggle(bool on) 
+        {
+            if (on) {this.Fill = Brushes.Red;} else {this.Fill = Brushes.White;}
+        }
+      
+    }
+}
---
+++ BinaryClock/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace BinaryClock
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private System.Timers.Timer m_renderTimer;
+        private AutoResetEvent m_renderSignal = new AutoResetEvent(false);
+        private List<Circle> _Circles = new List<Circle>();
+        private List<string> _ClockValues = new List<string>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            GridSetup();
+
+            m_renderTimer = new System.Timers.Timer(500);
+            m_renderTimer.Elapsed += (_, __) => m_renderSignal.Set();
+            m_renderTimer.Enabled = true;
+            Task.Factory.StartNew(() => RenderLoop(), TaskCreationOptions.LongRunning);
+
+        }
+
+        private void GridSetup()
+        {
+            for (int y = 0; y <= 5; y++) {
+                for (int i = 0; i <= 3; i++)
+                {
+                    Circle C = new Circle();
+                    _Circles.Add(C);
+                    Canvas1.Children.Add(C);
+                    Canvas.SetLeft(C, 50 + (y * 30));
+                    Canvas.SetTop(C, 50+ (i * 30));
+                } 
+            }
+
+            update();
+
+        }
+
+        private void RenderLoop()
+        {
+            while (true)
+            {
+                m_renderSignal.WaitOne();
+                Dispatcher.BeginInvoke((Action)update, DispatcherPriority.Send, null);
+            }
+        }
+
+        private void update()
+        {         
+           
+           _ClockValues.Clear();
+           string[] Values = DateTime.Now.ToString("hhmmss").Select(c => c.ToString()).ToArray();
+           lblBinaryValue.Content = DateTime.Now.ToString("hh:mm:ss");
+           foreach (string s in Values) {_ClockValues.Add(Convert.ToString(Convert.ToInt16(s), 2).PadLeft(4, '0'));}
+           for (int i = 0; i <= 5; i++) {updateClock(_ClockValues[i], i);}
+
+        }
+
+
+        private void updateClock(string value, int column) 
+        { 
+  
+         int i = 0; 
+           foreach (char s in value)
+           {
+               if (s.ToString() == "1") { _Circles[(column * 4) + i].toggle(true); }
+               else if (s.ToString() == "0"){ _Circles[(column * 4) + i].toggle(false); }
+               i++;
+           }
+
+        }
+
+    }
+}
